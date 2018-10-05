0117f9b3ebf3b59cdcd243ae1c9c83b11372189b
Thomas Schreiner <thomas.schreiner@fleischpruefring.de>
2018-10-05 16:23:38 +0200

    und weiter gehts

--- BomInator/Program.cs
+++ BomInator/Program.cs
@@ -3,67 +3,78 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace BomInator
 {
     class Program
     {
+        protected internal static byte[][] AnsiBytes = new byte[][] { new byte[] { 228 }, new byte[] { 246 }, new byte[] { 252 }, new byte[] { 223 } };
+
+        protected internal static byte[][] Utf8Bytes = new byte[][]
+        {
+            new byte[] {195,164},
+            new byte[] {195,182},
+            new byte[] {195,188},
+            new byte[] {195,159}
+        };
+
+        //-d=g:\BomInator\TestFiles -p=*.cs
         static void Main(string[] args)
         {
             var arguments = ParseArguments(args);
-            if (arguments.ListEncodings)
-            {
-                ListEncodings();
-            }
-
             IEnumerable<FileInfo> files = FindFiles(arguments.Directory, arguments.FileNamePattern);
             var bom = Encoding.UTF8.GetPreamble();
             byte[] fileBytes = new byte[0];
-            foreach (var file in files)
+
+            var bomFiles = files.Select(f => new BomFile() { FileInfo = f });
+
+            foreach (var file in bomFiles)
             {
-                if(file.Length > fileBytes.Length) fileBytes = new byte[file.Length];
+                if (file.Length > fileBytes.Length) fileBytes = new byte[file.Length];
                 using (FileStream fs = file.OpenRead())
                 {
                     fs.Read(fileBytes, 0, Convert.ToInt32(file.Length));
 
-                    //foreach (byte b1 in bom)
-                    //{
-                    //    Console.Write($"{Convert.ToInt16(b1),4}");
-                    //}
-                    //Console.Write(" | ");
-                    //foreach (byte b1 in b)
-                    //{
-                    //    Console.Write($"{Convert.ToInt16(b1),4}");
-                    //}
-                    //Console.Write(" | ");
                     var b = new byte[bom.Length];
                     Array.Copy(fileBytes, b, b.Length);
                     if (bom.SequenceEqual(b))
                     {
                         //Console.WriteLine($"match{file.FullName}");
+                        continue;
                     }
                     else
                     {
-                        //, {}, {}, {})
-                        
-                        if(FindBytes(fileBytes, file.Length,
-                            new byte[][] {new byte[] {228}, new byte[] {246}, new byte[] {252}, new byte[] {223}}))
-                            Console.Write("ANSI ");
-                        else if (FindBytes(fileBytes, file.Length,
-                            new byte[][]
+                        if (FindBytes(fileBytes, file.Length, AnsiBytes))
                         {
-                            new byte[] {195,164}, 
-                            new byte[] {195,182}, 
-                            new byte[] {195,188}, 
-                            new byte[] {195,159}
-                        }))
-                            Console.Write("UTF8 ");
-                        else 
-                            Console.Write("???? ");
-                        Console.WriteLine($"{file.FullName}");
+                            file.FoundEncodings.Add("iso-8859-1");
+                        }
+
+                        else if (FindBytes(fileBytes, file.Length, Utf8Bytes))
+                        {
+                            file.FoundEncodings.Add("utf-8");
+                        }
                     }
+                }
+
+                if (file.FoundEncodings.Count > 1)
+                {
+                    Console.WriteLine("multiencodings " + file.FullName);
+                }
+                else
+                {
+                    var mover = new FileInfo(file.FullName);
+                    mover.MoveTo(file.FullName + ".bak");
+
+                    var inputEncoding = Encoding.GetEncoding(file.FoundEncodings.FirstOrDefault() ?? "iso-8859-1");
+                    char[] chars = inputEncoding.GetChars(fileBytes, 0, Convert.ToInt32(file.Length));
+
+                    using(FileStream fs = file.FileInfo.OpenWrite())
+                    using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                    {
+                        sw.Write(chars);
+                    }
+
                 }
             }
             Console.ReadLine();
@@ -93,7 +104,6 @@ namespace BomInator
             {
                 Console.WriteLine($"{encoding.CodePage}\t{encoding.Name,-10}\t{encoding.DisplayName}");
             }
-            Environment.Exit(0);
         }
 
         private static CommandLineArgments ParseArguments(string[] args)
@@ -124,14 +134,26 @@ namespace BomInator
                 .WithDescription("");
             ICommandLineParserResult result = parser.Parse(args);
 
+            var arguments = parser.Object;
+            if (arguments.ListEncodings)
+            {
+                ListEncodings();
+                Exit();
+            }
+
             if (result.HelpCalled || result.HasErrors)
             {
                 parser.HelpOption.ShowHelp(parser.Options);
-                Environment.Exit(1);
+                Exit();
             }
-            var arguments = parser.Object;
 
             return arguments;
+        }
+
+        private static void Exit()
+        {
+            Console.ReadLine();
+            Environment.Exit(1);
         }
 
         private static IEnumerable<FileInfo> FindFiles(string directory, string pattern)
@@ -149,5 +171,23 @@ namespace BomInator
         public string FileNamePattern { get; set; }
         public string OriginalEncoding { get; set; }
         public bool Validate { get; set; }
+    }
+
+    internal class BomFile
+    {
+        public BomFile()
+        {
+            FoundEncodings = new List<string>();
+        }
+
+        public FileInfo FileInfo { get; set; }
+        public IList<string> FoundEncodings { get; }
+        public long Length => FileInfo.Length;
+        public string FullName => FileInfo.FullName;
+
+        public FileStream OpenRead()
+        {
+            return FileInfo.OpenRead();
+        }
     }
 }
